9d4bc0e76c02ce735f85b5aef901904c41fcdf37
Tahminur Rahman <trahman1@pride.hofstra.edu>
2021-05-15 23:53:49 -0400

    Have converted json data retrieved from api to stock data dictionary

--- stocks.py
+++ stocks.py
@@ -3,6 +3,26 @@ import json
 
 import config as cfg
 
-
+#later configure this respoinse status code to handle errors
 response = response = requests.get(cfg.api_url)
 print(response.status_code)
+
+
+### Function is to convert json returned from API into a dictionary with key being name or label, and values being price.
+def convertAPIDataToMarketData(data):
+
+    market = data['Markets'][0]
+    #dictionary to store allStocks with name and price key value pair
+    allstocks = {}
+
+    for stock in market:
+        allstocks[stock['Name']] = stock['Price']
+
+    return allstocks
+
+
+ListOfStocks = convertAPIDataToMarketData(response.json())
+
+
+#need to look into why the price is different than price on robinhood may need diff api :/
+print(ListOfStocks['Dogecoin'])
